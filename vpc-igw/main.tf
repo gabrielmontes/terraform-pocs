2ec3fcd45946b3bf8a77a4491dc2631d8e06ba49
Gabriel Montes Espinoza <gmontes@snapfinance.com>
2024-03-19 20:50:48 -0600

    Initial commit

---
+++ vpc-igw/main.tf
@@ -0,0 +1,59 @@
+# Data sources:
+
+locals {
+  azs_count = 2
+  azs_names = data.aws_availability_zones.available.names
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+
+# Resources:
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags                 = { Name = "main-vpc" }
+}
+
+resource "aws_subnet" "public" {
+  count                   = local.azs_count
+  vpc_id                  = aws_vpc.main.id
+  availability_zone       = local.azs_names[count.index]
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 10 + count.index)
+  map_public_ip_on_launch = true
+  tags                    = { Name = "main-public-${local.azs_names[count.index]}" }
+}
+
+# Internet gateway
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "main-igw" }
+}
+
+resource "aws_eip" "main" {
+  count      = local.azs_count
+  depends_on = [aws_internet_gateway.main]
+  tags       = { Name = "main-eip-${local.azs_names[count.index]}" }
+}
+
+# Public route table:
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "main-rt-public" }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = local.azs_count
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
