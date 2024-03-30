a5f400934ef597f53caff8c264eeb6fdfc460f2c
Gabriel Montes Espinoza <gmontes@snapfinance.com>
2024-03-29 18:29:44 -0600

    Added versioning.

--- s3/main.tf
+++ s3/main.tf
@@ -5,6 +5,13 @@ resource "aws_s3_bucket" "poc" {
     }
 }
 
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.poc.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_acl" "poc-acl" {
   bucket      = aws_s3_bucket.poc.id
   acl         = var.acl_type
